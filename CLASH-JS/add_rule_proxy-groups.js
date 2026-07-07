56d8341489c6a9d5c3689d92c7867f7ff10a5ca4
1faith1 <40625968+1faith1@users.noreply.github.com>
2026-07-07 17:39:50 +0800

    Update add_rule_proxy-groups.js

--- CLASH-JS/add_rule_proxy-groups.js
+++ CLASH-JS/add_rule_proxy-groups.js
@@ -14,7 +14,7 @@ const PROXY_GROUPS = [
   // === 单/多区域组合 - 分组创建 示例 - 自动进行延迟测试
   // 使用此模式match对应地址进行模糊搜索，即使没有也不影响使用
   {
-    name: "【日本-自动更新】",
+    name: "AAA - [日本-自动更新]",
     match: /(日本)/,
 	//  === 添加自动测速功能 START ===
 	interval: 3600, // 1小时测速一次
@@ -23,7 +23,7 @@ const PROXY_GROUPS = [
 	//  === 添加自动测速功能 END ===
   },
   {
-    name: "【美泰德-自动更新】",
+    name: "AAA - [美泰德-自动更新]",
     match: /(美国|泰国|德国)/,
 	// 添加自动测速功能
 	interval: 6, // 6秒测速一次
@@ -31,7 +31,7 @@ const PROXY_GROUPS = [
     url: "http://www.gstatic.com/generate_204",
   },
   {
-    name: "【新加坡-自动更新】",
+    name: "AAA - [新加坡-自动更新]",
     match: /(新加坡)/,
 	// 添加自动测速功能
 	interval: 6, // 6秒测速一次
@@ -39,7 +39,7 @@ const PROXY_GROUPS = [
     url: "http://www.gstatic.com/generate_204",
   },
   {
-    name: "【台湾-自动更新】",
+    name: "AAA - [台湾-自动更新]",
     match: /(台湾)/,
 	// 添加自动测速功能
 	interval: 6, // 6秒测速一次
@@ -47,7 +47,7 @@ const PROXY_GROUPS = [
     url: "http://www.gstatic.com/generate_204",
   },  
   {
-    name: "【香港-自动更新】",
+    name: "AAA - [香港-自动更新]",
     match: /(香港)/,
 	// 添加自动测速功能
 	interval: 6, // 6秒测速一次
