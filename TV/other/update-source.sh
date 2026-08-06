6a9dac7711a9071d2551a09fff2dbcca7aff414e
1faith1 <40625968+1faith1@users.noreply.github.com>
2026-08-06 15:49:25 +0800

    Update update-source.sh

--- TV/other/update-source.sh
+++ TV/other/update-source.sh
@@ -141,7 +141,7 @@ custom_yangweishi(){
   # 查找文本中"#============="所在行数
   FIND_ME_FILE_ONE_NUM=$(grep -En "#=====+" ${MY_SOURCE_FILE_NAME} |head -1|awk -F ":" '{print $1}')
   # ====
-  echo '添加其他源到文件中...'
+  echo "添加新group-title的源到${MY_SOURCE_FILE_NAME}文件中..."
   sed -i "${FIND_ME_FILE_ONE_NUM}r ${NEWFILE_2}" ${MY_SOURCE_FILE_NAME}
 }
 
