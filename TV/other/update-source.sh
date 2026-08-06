d20c5d66b116ba0b1b4dfbfc2a41f88d2c5e40cc
1faith1 <40625968+1faith1@users.noreply.github.com>
2026-08-06 15:15:27 +0800

    Update update-source.sh

--- TV/other/update-source.sh
+++ TV/other/update-source.sh
@@ -83,18 +83,19 @@ local_chuli_yuan_source(){
   ME_FILE_ONE_NUM=`echo $((${FIND_ME_FILE_ONE_NUM} + 1))`
   ME_FILE_TWO_NUM=`echo $((${FIND_ME_FILE_TWO_NUM} - 1))`
 
-  # 删除${meFile}旧数据
+  # 删除${MY_SOURCE_FILE_NAME}旧数据
+  echo "删除 ${MY_SOURCE_FILE_NAME}文件中的旧源数据"
   sed -i "${ME_FILE_ONE_NUM},${ME_FILE_TWO_NUM}d" ${MY_SOURCE_FILE_NAME}
 
   # ==================== 处理${NEW_SOURCE_FILE_NAME}文件数据
   NEW_FILE_NUM=$(grep -En  "TG频道" ${NEW_SOURCE_FILE_NAME}|grep "官网地址"|awk -F ":" '{print $1}')
   echo "处理${NEW_SOURCE_FILE_NAME}文件中数据，删除TG等无关信息"
-  sed -i "1d;${NEW_FILE_NUM},`echo $((${NEW_FILE_NUM} + 1))`d" ${NEW_SOURCE_FILE_NAME}
+  sed -i -e "1d" -e "${NEW_FILE_NUM},`echo $((${NEW_FILE_NUM} + 1))`d" ${NEW_SOURCE_FILE_NAME}
 
   # ====
-  echo "添加处理完成${NEW_SOURCE_FILE_NAME}中源到${MY_SOURCE_FILE_NAME}文件中"
+  echo "添加处理后的${NEW_SOURCE_FILE_NAME}文件中源到${MY_SOURCE_FILE_NAME}文件中"
   sed -i "${FIND_ME_FILE_ONE_NUM}r ${NEW_SOURCE_FILE_NAME}" ${MY_SOURCE_FILE_NAME}
-  echo '更新我的文件中时间信息'
+  echo '更新${MY_SOURCE_FILE_NAME}文件中时间信息'
   sed -i "s/更新时间.*/更新时间${NEW_DATE}/g" ${MY_SOURCE_FILE_NAME}
 }
 
