d3d17ef5a68e2001c393685163d8028d1e8f9a97
1faith1 <40625968+1faith1@users.noreply.github.com>
2026-08-06 15:43:56 +0800

    Update update-source.sh

--- TV/other/update-source.sh
+++ TV/other/update-source.sh
@@ -110,12 +110,12 @@ custom_yangweishi(){
   fi
   # 获取全部ys/ws的group-title并且去重
   GET_ALL_YANGWEISHI_GROUPTITLE=$(grep -Ei "cctv1|北京卫视|东方卫视|辽宁卫视" ${NEW_SOURCE_FILE_NAME} |\
-                                grep -E -o "group-title=.*\,"|sed 's/,//g'|sort|uniq|grep -Ev "体育|内网|ITV")
-
+                                grep -E -o "group-title=.*,"|sed 's/,//g'|sort|uniq|grep -Ev "体育|内网|ITV")
+  echo "本次新增group-title包含列表信息如下："
   # // 处理文件：根据上面GET_ALL_YANGWEISHI_GROUPTITLE进行遍历获取需要的ys/ws到NEWFILE_1文件
   for group_title in ${GET_ALL_YANGWEISHI_GROUPTITLE[@]}
   do
-    echo ${group_title}
+    echo "      ${group_title}"
     GET_GROUP_TATLE_VALUE=$(echo ${group_title}|awk -F "\"" '{print $2}')
     grep -A 1 -F -- ${GET_GROUP_TATLE_VALUE}  ${NEW_SOURCE_FILE_NAME} >>${NEWFILE_1}
   done
